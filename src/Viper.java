687b672f473f856e09a1f4cf3669d03b63bc8e67
SerzhSeryozhka <Jk3103KS@yandex.ru>
2024-04-05 16:57:31 +0300

    Создал класс и конструкт.

---
+++ src/Viper.java
@@ -0,0 +1,11 @@
+public class Viper extends Animal
+{
+    public Viper(int pawAmount, double weight) {
+        super(pawAmount, weight);
+    }
+
+    @Override
+    public void move() {
+
+    }
+}
