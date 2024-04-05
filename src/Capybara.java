d165c89e57fd979080525b104cc6a666ce3f842e
Anna Sold <anutka9602@mail.ru>
2024-04-05 16:52:06 +0300

    Создан класс капибара

---
+++ src/Capybara.java
@@ -0,0 +1,11 @@
+public class Capybara extends Mammal{
+
+    public Capybara(double weight) {
+        super(weight);
+    }
+
+    @Override
+    public void move() {
+        System.out.println("капибара бежит");
+    }
+}
